b33dcd903ff336b2bf002207c3baaa1c7447eace
Ori Klein <Ori Klein>
2016-12-26 12:06:39 +0200

    Fully functional filter. Yay! ^_^

--- src/App/events/components/event-filter/event-filter.component.js
+++ src/App/events/components/event-filter/event-filter.component.js
@@ -10,43 +10,65 @@ export default {
     }
   },
   methods: {
+    resetFilter() {
+      this.filter.str = '';
+      this.emitFilter();
+    },
     emitFilter() {
       let filtEvs = [];
       let filterBy = this.filter.focus.toLowerCase().substring(3);
 
       // If the filter input is empty than returns all the events in the database.
-      if(!this.filter.str){
+      if (this.filter.str === '') {
         filtEvs = this.eventsData;
       } else {
 
         let mutableSrc;
-        filtEvs = this.eventsData.filter( (event) => {
-        switch(filterBy) {
-          
-          case 'name':
-          mutableSrc = event.name;        
-          break;
+        filtEvs = this.eventsData.filter((event) => {
+          switch (filterBy) {
 
-          case 'venue':
-          mutableSrc = event.venue.name + " " + event.venue.address_1 + " " + event.venue.city + " " + event.venue.country + " " + event.venue.localized_country_name;
-          break;
+            case 'name':
+              mutableSrc = event.name;
+              break;
 
-          case 'date':
-          mutableSrc = event.time;
-          break;
-        }
+            case 'venue':
+              mutableSrc = event.venue.name + " " + event.venue.address_1 + " " + event.venue.city + " " + event.venue.country + " " + event.venue.localized_country_name;
+              break;
+
+            case 'date':
+              mutableSrc = event.time;
+              break;
+          }
+          // RegEx that checks for numbers in a string.
+          //(/\d/.test(this.filter.str))
+
+          if (filterBy != 'date') {
             return (mutableSrc.toLowerCase().indexOf(this.filter.str.toLowerCase()) > -1);
-          });
-        
+          } else {
+            let eventDate = new Date(mutableSrc);
+            let srcDay = 1+ eventDate.getDay() + '';
+            let srcMonth = 1+ eventDate.getMonth() + '';
+            let srcYear = 1+ eventDate.getFullYear() + '';
+            var filterDate = this.filter.str.split('-');
+            let tgtDay = filterDate[2].slice(1) + '';
+            let tgtMonth = filterDate[1] + '';
+            let tgtYear = filterDate[0] + '';
+            console.log('Event = Day: ', srcDay, ' Month: ', srcMonth, ' Year: ', srcYear);
+            console.log('Filter = Day: ', tgtDay, ' Month: ', tgtMonth, ' Year: ', tgtYear);
+            return ((srcDay === tgtDay) &&
+                    (srcMonth === tgtMonth) &&
+                    (srcYear === tgtYear)
+            );
+          }
+
+        });
       }
       this.$emit('filterChanged', filtEvs);
     }
   },
-
-  watch:{
-    eventsData: function(){
-        this.emitFilter();
-    }
+  watch: {
+    eventsData: function () {
+      this.emitFilter();
+    },
   }
 }
-
